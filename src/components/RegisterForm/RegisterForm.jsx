319147f2a62eb62ecb236081e722081c8ceb769e
João Paulo Oliveira <joaolhplira@gmail.com>
2022-03-08 13:49:42 -0300

    events and properties

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,18 +2,32 @@ import React, { Component } from "react";
 import "./style.css"
 
 export class RegisterForm extends Component {
+
+    constructor() {
+        super()
+        this.title = ""
+    }
+
+    handleTitleChange(event) {
+        this.title = event.target.value
+        console.log(this.title)
+    }
+
     render() {
         return (
             <form className="register-form">
                 <input 
                     type='text' 
                     placeholder='title' 
-                    className="register-form_input"/>
+                    className="register-form_input"
+                    onChange={this.handleTitleChange.bind(this)}
+                />
                     
                 <textarea 
                     placeholder='write your note...' 
                     className="register-form_input"
-                    rows={15}/>
+                    rows={15}
+                />
 
                 <button 
                     className="register-form_input register-form_submit">
